45dafab2301b61d2a43ebb3f338d452842058add
mono57 <aymaramono@gmail.com>
2020-09-07 13:46:34 +0100

    update comities list template

--- network/views.py
+++ network/views.py
@@ -136,6 +136,9 @@ class NewsListView(ListView):
     template_name = 'network/news-list.html'
     paginate_by = 6
 
+    # def get_queryset(self):
+    #     return NetworkNews.objects.all()
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = 'News'
