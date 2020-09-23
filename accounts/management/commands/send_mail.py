6361041c1cd9676337912d471615d1acb6350b8e
mono57 <aymaramono@gmail.com>
2020-09-23 22:31:06 +0100

    add send_mail command

--- accounts/management/commands/send_mail.py
+++ accounts/management/commands/send_mail.py
@@ -1,6 +1,10 @@
 from accounts.admin import send_html_email
+from django.core.mail import send_mail
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+from django.urls import reverse
+from django.conf import settings
+from django.template.loader import get_template
 
 
 User = get_user_model()
@@ -13,12 +17,24 @@ class Command(BaseCommand):
             context = {
                 'full_name': user.get_full_name(),
                 'email': user.email,
-                'password': 'ensaialumni'
+                'password': 'ensaialumni',
+                'login_url': reverse('account_login')
             }
-            
+
             subject = 'Confirmation'
             template_path = 'account/email_member.html'
 
-            send_html_email([user.email], subject, template_path, context)
+            html_email = get_template(template_path).render(context=context) 
+
+            # send_html_email([user.email], subject, template_path, context)
+
+            send_mail(
+                subject=subject,
+                message='', 
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list = [user.email,],
+                fail_silently = True,
+                html_message = html_email
+            )
 
             self.stdout.write("Mail sended to : {}".format(user.email))
